66399bf1aee0c3cb0ebdf769f2a5e171bf669d02
HUNTERG5100 <hunterg5100@gmail.com>
2021-06-11 11:02:16 -0500

    Added 'Task' model

--- todo/todo_app/models.py
+++ todo/todo_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=100)
+    details = models.TextField()
+    priority = models.IntegerField()
