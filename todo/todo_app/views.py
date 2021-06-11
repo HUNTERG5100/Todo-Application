03ea53b47864bda05ae033d54fcba31f5a8bce67
HUNTERG5100 <hunterg5100@gmail.com>
2021-06-11 10:47:58 -0500

    Updated views.py

--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+import datetime
+
 
 def index(request):
-    context = {}
+    context = {"current_date": datetime.date.today()}
     return render(request, "index.html", context)
